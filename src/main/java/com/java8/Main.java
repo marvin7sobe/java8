f615f62bb62a7ab98749ffed10d94abaf12429e0
Vladyslav Shynkarenko <vadic7wee2@gmail.com>
2015-01-09 13:57:27 +0200

    Added initial lambdas examples

---
+++ src/main/java/com/java8/Main.java
@@ -0,0 +1,34 @@
+package main.java.com.java8;
+
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args){
+        Main main = new Main();
+
+        String[] words = {"xyzwq", "aaa", "ab", "aabc34rer"};
+        main.doSortByLabmda(words);
+    }
+
+    public void doSortByLabmda(String[] words) {
+        System.out.println("Original array: " + Arrays.toString(words));
+
+        String[] wordsCopy = new String[words.length];
+        System.arraycopy(words, 0, wordsCopy, 0, words.length);
+
+        Comparator<String> cmp = (first, second) -> Integer.compare(first.length(), second.length());
+        Arrays.sort(wordsCopy, cmp);
+        System.out.println("Sorted array by word length v1: " + Arrays.toString(wordsCopy));
+
+        Arrays.sort(wordsCopy, (first, second) -> Integer.compare(first.length(), second.length()));
+        System.out.println("Sorted array by word length v2: " + Arrays.toString(wordsCopy));
+
+        Arrays.sort(wordsCopy, String::compareToIgnoreCase);
+        System.out.println("Sorted array alphabetically: " + Arrays.toString(wordsCopy));
+
+        Arrays.sort(wordsCopy, Comparator.comparing(String::length));
+        System.out.println("Sorted array by word length v3: " + Arrays.toString(wordsCopy));
+    }
+}
