45abcfb77cd80142ed40b13757cc3ca99f3a4508
Vladyslav Shynkarenko <vadic7wee2@gmail.com>
2015-01-10 23:19:30 +0200

    Example for UnaryOperator (on JavaFX Image)

--- src/main/java/com/java8/MyLambdas.java
+++ src/main/java/com/java8/MyLambdas.java
@@ -1,7 +1,13 @@
 package main.java.com.java8;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+
+import java.io.*;
 import java.util.function.IntConsumer;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,6 +18,7 @@ public class MyLambdas {
     public void executeTests() {
         testBasicSupplier();
         testIntConsumer();
+        testBasicUnaryOperator();
     }
 
     public void testBasicSupplier() {
@@ -43,5 +50,63 @@ public class MyLambdas {
         for (int i = 0; i < times; i++) {
             action.run();
         }
+    }
+
+    public void testBasicUnaryOperator() {
+        printMessage("\nBasic Image transformation using UnaryOperator:");
+        Image image = new Image(getPreparedImageFis());
+        Image brighter = transform(image, Color::brighter);
+        Utils.saveImage(brighter, "jpg", "eiffel-tower-brighter.jpg");
+
+        Image brighterByFactor = transform(image, brighter(1.8f));
+        Utils.saveImage(brighterByFactor, "jpg", "eiffel-tower-brighter-by-factor.jpg");
+    }
+
+    private FileInputStream getPreparedImageFis() {
+        File origin = new File("src/main/resources/eiffel-tower.jpg");
+        File result = new File("eiffel-tower.jpg");
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        try {
+            fis = new FileInputStream(origin);
+            fos = new FileOutputStream(result);
+            byte[] buf = new byte[1024];
+            int length;
+            while ((length = fis.read(buf)) > 0) {
+                fos.write(buf, 0, length);
+            }
+            return new FileInputStream(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) try {
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (fos != null) try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    private UnaryOperator<Color> brighter(float factor) {
+        return c -> c.deriveColor(0, 1, factor, 1);
+    }
+
+    private Image transform(Image image, UnaryOperator<Color> f) {
+        int width = (int) image.getWidth();
+        int height = (int) image.getHeight();
+        WritableImage output = new WritableImage(width, height);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                output.getPixelWriter().setColor(x, y,
+                        f.apply(image.getPixelReader().getColor(x, y)));
+            }
+        }
+        return output;
     }
 }
--- src/main/java/com/java8/Utils.java
+++ src/main/java/com/java8/Utils.java
@@ -1,7 +1,22 @@
 package main.java.com.java8;
 
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
 public class Utils {
     static void printMessage(String m) {
         System.out.println(m);
+    }
+
+    public static void saveImage(Image i, String extension, String pathname)  {
+        try {
+            ImageIO.write(SwingFXUtils.fromFXImage(i, null), extension, new File(pathname));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
